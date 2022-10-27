09f2276929e93d15804756489bcdab8e5c0fa4df
Meagan Lang <langmm.astro@gmail.com>
2022-10-27 15:03:05 -0400

    More debug messages

--- geometry.cpp
+++ geometry.cpp
@@ -837,6 +837,7 @@ static PyObject* ply_as_dict(PyObject* self, PyObject* args, PyObject* kwargs) {
     if (out == NULL)
 	return NULL;
     if (v->ply->comments.size() > 0) {
+	std::cerr << "comments" << std::endl;
 	PyObject* commentStr = PyUnicode_FromString("comment");
 	if (commentStr == NULL) {
 	    Py_DECREF(out);
@@ -861,6 +862,7 @@ static PyObject* ply_as_dict(PyObject* self, PyObject* args, PyObject* kwargs) {
 	}
 	Py_DECREF(comments);
     }
+    std::cerr << "non comments: " << v->ply->element_order.size() << std::endl;
     for (std::vector<std::string>::const_iterator it = v->ply->element_order.begin(); it != v->ply->element_order.end(); it++) {
 	std::map<std::string,PlyElementSet>::const_iterator eit = v->ply->elements.find(*it);
 	if (eit == v->ply->elements.end()) continue;
