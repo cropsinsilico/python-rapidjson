03a5f7f567fbc81266de988291dd053d31d5b9fa
Meagan Lang <langmm.astro@gmail.com>
2024-01-12 19:44:35 -0500

    Update ygg_options.cmake Add this branch to those the workflow runs for Move command line options and configuration in setup.py to CMakeLists.txt

--- run_script.sh
+++ run_script.sh
@@ -1,10 +1,11 @@
 set -e
-# export ASAN_OPTIONS=symbolize=1
-# export ASAN_SYMBOLIZER_PATH=$(which llvm-symbolizer)
-## python setup.py develop --rj-include-dir=../rapidjson/include/ --with-asan
-pip install -v -e .
-# pip install --rj-include-dir=../rapidjson/include/ -e . # --with-asan -e .
-## export DYLD_INSERT_LIBRARIES=/Users/langmm/mambaforge/envs/pyrj/lib/clang/14.0.6/lib/darwin/libclang_rt.asan_osx_dynamic.dylib
-# export DYLD_INSERT_LIBRARIES=$(clang -print-file-name=libclang_rt.asan_osx_dynamic.dylib)
+export ASAN_OPTIONS=symbolize=1
+export ASAN_SYMBOLIZER_PATH=$(which llvm-symbolizer)
+pip install --config-settings=cmake.define.RAPIDJSON_INCLUDE_DIRS=../rapidjson/include/ \
+    --config-settings=cmake.define.YGG_BUILD_ASAN:BOOL=ON \
+    --config-settings=cmake.define.YGG_BUILD_UBSAN:BOOL=ON \
+    -v -e .
+# export DYLD_INSERT_LIBRARIES=/Users/langmm/mambaforge/envs/pyrj/lib/clang/14.0.6/lib/darwin/libclang_rt.asan_osx_dynamic.dylib
+export DYLD_INSERT_LIBRARIES=$(clang -print-file-name=libclang_rt.asan_osx_dynamic.dylib)
 python -m pytest -svx tests/
-# make -C docs doctest -e PYTHON=$(which python3) -e DYLD_INSERT_LIBRARIES=$(clang -print-file-name=libclang_rt.asan_osx_dynamic.dylib)
+make -C docs doctest -e PYTHON=$(which python3) -e DYLD_INSERT_LIBRARIES=$(clang -print-file-name=libclang_rt.asan_osx_dynamic.dylib)
